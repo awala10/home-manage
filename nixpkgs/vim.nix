e38dd87d7f354ac00e85a921f0fd78c9a719c04b
awala10 <khalad.awala@gmail.com>
2023-02-06 17:01:38 +0100

    format settings

--- nixpkgs/vim.nix
+++ nixpkgs/vim.nix
@@ -14,34 +14,34 @@ pkgs:
   ];
 
   extraConfig = ''
-  syntax on
-  set background=dark
-  filetype plugin indent on
-  set nocompatible
-  set hlsearch
-  set autoread
-  set expandtab
-  set autoindent
-  set number relativenumber
-  set path+=**
-  set ruler
-  set wildmenu
-  set noswapfile
-  set showmatch
-  set splitright
-  au BufRead,BufNewFile *.yml,*.yaml set filetype=yaml
-  let mapleader=","
-  set cursorline
+    syntax on
+    set background=dark
+    filetype plugin indent on
+    set nocompatible
+    set hlsearch
+    set autoread
+    set expandtab
+    set autoindent
+    set number relativenumber
+    set path+=**
+    set ruler
+    set wildmenu
+    set noswapfile
+    set showmatch
+    set splitright
+    au BufRead,BufNewFile *.yml,*.yaml set filetype=yaml
+    let mapleader=","
+    set cursorline
 
-  nmap <leader>w :wq<CR>
-  nmap <leader>! :q!<CR>
-  set list listchars+=eol:¬,trail:¶
-  let &t_SI="\e[6.q"
-  let &t_EI="\e[1.q"
-  autocmd BufWritePre * %s/\s\+$//e
+    nmap <leader>w :wq<CR>
+    nmap <leader>! :q!<CR>
+    set list listchars+=eol:¬,trail:¶
+    let &t_SI="\e[6.q"
+    let &t_EI="\e[1.q"
+    autocmd BufWritePre * %s/\s\+$//e
 
-  let g:airline_powerline_forts = 1
-  let g:airline_theme='dark'
-  colorscheme gruvbox
-    '';
+    let g:airline_powerline_forts = 1
+    let g:airline_theme='dark'
+    colorscheme gruvbox
+      '';
   }
