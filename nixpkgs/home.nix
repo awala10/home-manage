943cbe240277172b6967aa27c3640315db571fd9
awala10 <khalad.awala@gmail.com>
2023-02-06 16:02:56 +0100

    home re-arrange

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -7,9 +7,12 @@ in
 {
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
-  home.username = "awala";
-  home.homeDirectory = "/home/awala";
-
+  home = {
+    username = "awala";
+    homeDirectory = "/home/awala";
+    packages = packages;
+    stateVersion = "22.11";
+  };
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
@@ -18,12 +21,10 @@ in
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "22.11";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  home.packages = packages;
   programs.bash = bashsettings;
   programs.vim = vimsettings pkgs;
 #  programs.zsh = {
